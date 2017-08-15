058a88f42e6eb661074d738879cb4e17bb5b83db
Seyedamirhossein Hesamian <amir734jj@live.com>
2017-08-14 22:28:22 -0500

    email text

--- modules/email.js
+++ modules/email.js
@@ -56,14 +56,27 @@ module.exports = function(smtpTransport, rootURL) {
       var mail = {
         from: "Milwaukee Internationals <tourofmilwaukee@gmail.com>",
         to: driver.email,
-        subject: "Tour of Milwaukee - Confirmation",
-        text: "Email confirmation for the Tour of Milwaukee",
-        html: _.template("                                                                                \
-        <ul>                                                                                              \
-        <% _.each(students, function(student){ %>                                                         \
-            <li><%= student.fullname %></li>                                                              \
-        <% }); %>                                                                                         \
-        </ul>                                                                                             \
+        subject: "Tour of Milwaukee - Assigned Students",
+        text: "Re Tour of Milwaukee",
+        html: _.template("                                                      \
+        <p> This is an automatically generated email. </p> \
+        <p> ----------------------------------------- </p> \
+        <br>                                    \
+        <p> Your Driver ID: <%= driver.displayId %></p>    \
+        <ul>                                                                    \
+        <% _.each(students, function(student){ %>                               \
+            <li><%= student.fullname %> (<%= student.country %>)</li>                    \
+            <br>                                                                \
+        <% }); %>                                                               \
+        </ul>                                                                   \
+        <br>                                    \
+        <br>                                    \
+        <p> Host Name: <%= host.fullname %> </p>    \
+        <p> Host Name: <%= host.address %> </p>    \
+        <br>                                    \
+        <br>                                    \
+        <p> Thank you for helping out this year. Reply to this email will be sent automatically to the team.</p> \
+        <p> If you have any question or comment, please contact Asher Imtiaz (414-499-5360) or Marie Wilke (414-852-5132).</p> \
         ")({
           students: driver.students
         })
@@ -84,9 +97,12 @@ module.exports = function(smtpTransport, rootURL) {
       var mail = {
         from: "Milwaukee Internationals <tourofmilwaukee@gmail.com>",
         to: host.email,
-        subject: "Tour of Milwaukee - Confirmation",
+        subject: "Tour of Milwaukee - Assigned Drivers",
         text: "Email confirmation for the Tour of Milwaukee",
         html: _.template("                                                                                \
+        <p> This is an automatically generated email. </p> \
+        <p> ----------------------------------------- </p> \
+        <br>                                    \
         <% if (drivers.length == 0) { %>                                                                  \
             <p>No driver is assigned to your home.</p>                                                    \
         <% } %>                                                                                           \
@@ -97,6 +113,9 @@ module.exports = function(smtpTransport, rootURL) {
             <% }); %>                                                                                     \
         <% } %>                                                                                           \
         </ul>                                                                                             \
+        <br>                                    \
+        <p> Thank you for hosting this year. Reply to this email will be sent automatically to the team.</p> \
+        <p> If you have any question or comment, please contact Asher Imtiaz (414-499-5360) or Marie Wilke (414-852-5132).</p> \
         ")({
           drivers: host.drivers
         })
