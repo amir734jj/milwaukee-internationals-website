9e08f6519b7ee0d17147e346eb406239208cc601
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2017-08-23 20:49:36 -0500

    WIP

--- models/utilityCtrl.js
+++ models/utilityCtrl.js
@@ -8,9 +8,35 @@ module.exports = function(databaseModels, db, injectTo) {
   var key = "utilityCtrl";
 
   var methods = {
+      getAll: function(callback){
+        var userCtrl = require('../models/userCtrl')(databaseModels.userModel, db, []);
+        var studentCtrl = require('../models/studentCtrl')(databaseModels, db, []);
+        var personCtrl = require('../models/personCtrl')(databaseModels, db, []);
+
+        var retVal = {};
+
+        userCtrl.getAllUsers((users) => {
+          retVal["users"] = users;
+
+          studentCtrl.getAllStudents((students) => {
+            retVal["students"] = students;
+
+            personCtrl.getPersonByRole("driver", (drivers) => {
+              retVal["drivers"] = drivers;
+
+              personCtrl.getPersonByRole("host", (hosts) => {
+                retVal["hosts"] = hosts;
+
+                callback(retVal);
+              });
+
+            });
+
+          });
+        });
 
 
-
+      }
   };
 
   /* inject controller to routers */
--- routes/utility.js
+++ routes/utility.js
@@ -1,16 +1,53 @@
 var authentication = require('../modules/authentication');
 var express = require('express');
 var router = express.Router();
+var _ = require('underscore');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('utility/email');
+  authentication(req, function loggedIn() {
+    res.render('utility/email');
+  }, function loggedOut() {
+    res.redirect("/");
+  });
 });
 
 router.post('/email', function(req, res, next) {
   authentication(req, function loggedIn() {
-    global.sendMailUtility(["hesamian@uwm.edu", "asherimtiaz@gmail.com"], req.body);
-    res.redirect("/utility");
+    var emailList = [];
+
+    router.utilityCtrl.getAll((retVal) => {
+        if (req.body.hosts) {
+          emailList = emailList.concat(_.pluck(retVal.hosts, "email"));
+        }
+
+        if (req.body.drivers) {
+          emailList = emailList.concat(_.pluck(retVal.drivers, "email"));
+        }
+
+        if (req.body.users) {
+          emailList = emailList.concat(_.pluck(retVal.users, "email"));
+        }
+
+        if (req.body.students) {
+          emailList = emailList.concat(_.pluck(retVal.students, "email"));
+        }
+
+        // global.sendMailUtility(emailList, req.body);
+
+        res.redirect("/utility");
+    });
+  }, function loggedOut() {
+    res.redirect("/");
+  });
+});
+
+
+router.get('/email/json', function(req, res, next) {
+  authentication(req, function loggedIn() {
+    router.utilityCtrl.getAll((retVal) => {
+      res.json(retVal);
+    });
   }, function loggedOut() {
     res.redirect("/");
   });
